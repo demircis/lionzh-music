c8dad271ba99ca5b6c9b50d522e394ec5c27d8bc
Sinan Demirci <demirci.sinan96@gmail.com>
2021-09-23 23:46:06 +0200

    -add queueing of songs -handle state transitions

--- musicSubscription.js
+++ musicSubscription.js
@@ -1,34 +1,87 @@
-const { createAudioPlayer, AudioPlayerStatus } = require('@discordjs/voice');
+const { createAudioPlayer, AudioPlayerStatus, VoiceConnectionStatus, entersState, VoiceConnectionDisconnectReason } = require('@discordjs/voice');
 
-const timeout = 60000;
+const idleTimeout = 15e3;
+const waitTimeout = 5e3;
 
 module.exports = class MusicSubscription {
     voiceConnection;
     audioPlayer;
+    queue;
 
     constructor(voiceConnection) {
         this.voiceConnection = voiceConnection;
-        this.voiceConnection.on('stateChange', (oldState, newState) => {
+        this.voiceConnection.on('stateChange', async (oldState, newState) => {
             console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
+            if (newState.status == VoiceConnectionStatus.Disconnected) {
+                if (newState.reason == VoiceConnectionDisconnectReason.WebSocketClose && newState.closeCode === 4014) {
+                    try {
+                        await entersState(this.voiceConnection, VoiceConnectionStatus.Connecting, waitTimeout);
+                    } catch {
+                        this.voiceConnection.destroy();
+                    }
+                } else if (this.voiceConnection.rejoinAttempts < 5) {
+                    setTimeout(() => {}, (this.voiceConnection.rejoinAttempts + 1) * waitTimeout);
+                    this.voiceConnection.rejoin();
+                } else {
+                    this.voiceConnection.destroy();
+                }
+            } else if (newState.status == VoiceConnectionStatus.Signalling || newState.status == VoiceConnectionStatus.Connecting) {
+                try {
+                    await entersState(this.voiceConnection, VoiceConnectionStatus.Ready, waitTimeout);
+                } catch {
+                    if (this.voiceConnection.state.status != VoiceConnectionStatus.Destroyed) this.voiceConnection.destroy();
+                }
+            } else if (newState.status == VoiceConnectionStatus.Destroyed) {
+                this.stop();
+            }
         });
         
         this.audioPlayer = createAudioPlayer();
-        this.voiceConnection.subscribe(this.audioPlayer);
-        this.audioPlayer.on('error', error => {
-            console.error(error.stack);
-        });
         this.audioPlayer.on('stateChange', (oldState, newState) => {
             console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
+            if (oldState.status != AudioPlayerStatus.Idle && newState.status == AudioPlayerStatus.Idle) {
+                this.processQueue();
+            }
         });
-        this.audioPlayer.on(AudioPlayerStatus.Idle, () => setTimeout(() => this.voiceConnection.destroy(), timeout));
+/*         this.audioPlayer.on(AudioPlayerStatus.Idle, async () => {
+            try {
+                await entersState(this.audioPlayer, AudioPlayerStatus.Playing, idleTimeout);
+            } catch {
+                this.voiceConnection.disconnect();
+            }
+        }); */
+        this.audioPlayer.on('error', error => {
+            console.error(error);
+        });
+
+        this.voiceConnection.subscribe(this.audioPlayer);
+        this.queue = [];
     }
 
-    playTrack(audioResource) {
-        this.audioPlayer.play(audioResource);
+    async enqueue(track) {
+        this.queue.push(track);
+        await this.processQueue();
     }
 
-    leaveChannel() {
-        this.voiceConnection.destroy();
+    stop() {
+        this.queue = [];
+        this.audioPlayer.stop(true);
+    }
+
+    async processQueue() {
+        if (this.audioPlayer.state.status != AudioPlayerStatus.Idle || this.queue.length == 0) {
+            return;
+        }
+
+        const track = this.queue.shift();
+        try {
+            const resource = await track.createAudioResource();
+            this.audioPlayer.play(resource);
+        } catch(error) {
+            console.log(error);
+            await this.processQueue();
+        }
+        
     }
 
 }
--- playImpl.js
+++ playImpl.js
@@ -1,48 +1,65 @@
 const { Interaction, Message, GuildMember } = require("discord.js");
-const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
+const { joinVoiceChannel, getVoiceConnection, entersState, VoiceConnectionStatus } = require('@discordjs/voice');
 const MusicSubscription = require('./musicSubscription');
 const RequestedTrack = require('./requestedTrack');
 const embedCreator = require('./embedCreator');
 
+const subscriptions = new Map();
+
 module.exports = {
     async play(command, url) {
-        var requester = null;
-        const voiceChannel = command.member.voice.channel;
-        if (command instanceof Interaction) {
-            var interaction = command;
-            requester = interaction.user;
-            if (!voiceChannel) {
-                await interaction.reply({ embeds: [embedCreator.createErrorMessageEmbed('You need to be in a voice channel first!')] });
-                return;
+        let requester = command instanceof Interaction ? command.user : command.author;
+        let subscription = subscriptions.get(command.guildId);
+        if (!subscription) {
+            let voiceChannel = command.member.voice.channel;
+            if (command instanceof Interaction) {
+                let interaction = command;
+                if (!voiceChannel) {
+                    await interaction.reply({ embeds: [embedCreator.createErrorMessageEmbed('You need to be in a voice channel first!')] });
+                    return;
+                }
+                if (!interaction.member instanceof GuildMember) {
+                    await interaction.reply({ embeds: [embedCreator.createErrorMessageEmbed('You are not a member of this server!')] });
+                    return;
+                }
+            } else if (command instanceof Message) {
+                let message = command;
+                if (!voiceChannel) {
+                    message.channel.send({ embeds: [embedCreator.createErrorMessageEmbed('You need to be in a voice channel first!')] });
+                    return;
+                }
+                if (!message.member instanceof GuildMember) {
+                    message.channel.send({ embeds: [embedCreator.createErrorMessageEmbed('You are not a member of this server!')] });
+                    return;
+                }
             }
-            if (!interaction.member instanceof GuildMember) {
-                await interaction.reply({ embeds: [embedCreator.createErrorMessageEmbed('You are not a member of this server!')] });
-            }
-        } else if (command instanceof Message) {
-            var message = command;
-            requester = message.author;
-            if (!voiceChannel) {
-                message.channel.send({ embeds: [embedCreator.createErrorMessageEmbed('You need to be in a voice channel first!')] });
-                return;
-            }
-            if (!message.member instanceof GuildMember) {
-                message.channel.send({ embeds: [embedCreator.createErrorMessageEmbed('You are not a member of this server!')] });
-            }
-        }
-        const connection = getVoiceConnection(voiceChannel.guild.id);
-        if (!connection) {
-            var subscription = new MusicSubscription(joinVoiceChannel({
+            subscription = new MusicSubscription(joinVoiceChannel({
                 channelId: voiceChannel.id,
                 guildId: voiceChannel.guild.id,
                 adapterCreator: voiceChannel.guild.voiceAdapterCreator,
             }));
+            subscriptions.set(command.guildId, subscription);
         }
+
+        try {
+            await entersState(subscription.voiceConnection, VoiceConnectionStatus.Ready, 5000);
+        } catch(error) {
+            console.log(error);
+            if (command instanceof Interaction) {
+                await command.reply({ embeds: [embedCreator.createErrorMessageEmbed('Failed to connect to voice channel!')] });
+                return;
+            } else if (command instanceof Message) {
+                command.channel.send({ embeds: [embedCreator.createErrorMessageEmbed('Failed to connect to voice channel!')] });
+                return;
+            }
+        }
+        
         const track = await RequestedTrack.from(url, requester);
-        subscription.playTrack(await track.createAudioResource());
+        subscription.enqueue(track);
         if (command instanceof Interaction) {
-            await interaction.reply({ embeds: [embedCreator.createTrackInfoEmbed(track)] });
+            await command.reply({ embeds: [embedCreator.createTrackInfoEmbed(track)] });
         } else if (command instanceof Message) {
-            message.channel.send({ embeds: [embedCreator.createTrackInfoEmbed(track)] });
+            command.channel.send({ embeds: [embedCreator.createTrackInfoEmbed(track)] });
         }
 	}
 }
